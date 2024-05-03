ef0ac8abeca96c8c6ceb68782ffe0ec401fc3230
정서희 <jsh001505@naver.com>
2024-05-03 14:24:43 +0900

    Feat: layout 중 header publish 추가

---
+++ layouts/default.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { type MenuProps } from "ant-design-vue";
+
+const route = useRoute();
+const current = ref<string[]>([route.path]);
+const items = ref<MenuProps["items"]>([
+  {
+    key: "/docs",
+    label: "Docs",
+    title: "Docs",
+  },
+  {
+    key: "/components",
+    label: "Components",
+    title: "Components",
+  },
+]);
+</script>
+
+<template>
+  <a-page-header
+    title="vue3-kakao-maps"
+    :avatar="{ src: '/images/logo.png', shape: 'square' }"
+    class="page-header"
+  >
+    <template #extra>
+      <a-menu v-model:selectedKeys="current" mode="horizontal" :items="items" />
+      <a-divider type="vertical"></a-divider>
+      <a-switch></a-switch>
+      <a-divider type="vertical"></a-divider>
+      <img src="/images/npmLogo.png" />
+    </template>
+  </a-page-header>
+
+  <slot />
+</template>
+
+<style lang="scss">
+.page-header {
+  padding: 0 2rem;
+}
+.ant-space-horizontal {
+  height: 100%;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  modules: ["@ant-design-vue/nuxt"],
+  devtools: { enabled: true },
+  antd: {
+    // Options
+  },
+});
