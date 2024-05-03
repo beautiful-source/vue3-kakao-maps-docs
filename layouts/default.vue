cc32b2e8612cc2703f67b220f0a124840ca1ef1b
정서희 <jsh001505@naver.com>
2024-05-03 17:54:18 +0900

    Feat: 다크 테마 적용

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 const route = useRoute();
+const colorMode = useColorMode();
 const headerMenuItems = [
   {
     key: "/docs",
@@ -12,36 +13,47 @@ const headerMenuItems = [
     selected: route.path === "/components",
   },
 ];
+const onClickThemeSwitch = () => {
+  colorMode.preference = colorMode.value === "light" ? "dark" : "light";
+};
 </script>
 
 <template>
-  <a-page-header
-    title="vue3-kakao-maps"
-    :avatar="{ src: '/images/logo.png', shape: 'square' }"
-    class="page-header"
-  >
-    <template #extra>
-      <ul>
-        <li
-          v-for="menuItem of headerMenuItems"
-          :key="menuItem.key"
-          :class="{
-            selected: menuItem.selected,
-          }"
-        >
-          <a :href="menuItem.key">
-            {{ menuItem.label }}
-          </a>
-        </li>
-      </ul>
-      <a-divider type="vertical"></a-divider>
-      <a-switch></a-switch>
-      <a-divider type="vertical"></a-divider>
-      <img src="/images/npmLogo.png" />
-    </template>
-  </a-page-header>
+  <div>
+    <a-page-header
+      title="vue3-kakao-maps"
+      :avatar="{ src: '/images/logo.png', shape: 'square' }"
+      class="page-header"
+    >
+      <template #extra>
+        <ul>
+          <li
+            v-for="menuItem of headerMenuItems"
+            :key="menuItem.key"
+            :class="{
+              selected: menuItem.selected,
+            }"
+          >
+            <a :href="menuItem.key">
+              {{ menuItem.label }}
+            </a>
+          </li>
+        </ul>
+        <a-divider type="vertical"></a-divider>
+        <a-switch
+          :checked="colorMode.value === 'dark'"
+          @click="onClickThemeSwitch"
+        ></a-switch>
+        <a-divider type="vertical"></a-divider>
+        <img src="/images/npmLogo.png" />
+      </template>
+    </a-page-header>
+    <div>
+      <layout-docs-menu />
+    </div>
 
-  <slot />
+    <slot />
+  </div>
 </template>
 
 <style lang="scss">
@@ -53,9 +65,8 @@ const headerMenuItems = [
     padding: 0;
     gap: 2rem;
     a {
-      color: black;
       &:hover {
-        color: $primary4;
+        color: $primary-4;
       }
     }
     .selected {
@@ -63,6 +74,25 @@ const headerMenuItems = [
       a {
         color: $primary;
       }
+    }
+  }
+}
+
+.dark-mode {
+  .page-header {
+    .ant-page-header-heading-title {
+      color: $gray-1;
+    }
+    a {
+      color: $gray-7;
+    }
+  }
+}
+
+.light-mode {
+  .page-header {
+    a {
+      color: $gray-11;
     }
   }
 }
