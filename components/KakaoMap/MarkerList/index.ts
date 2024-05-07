eaa5e786c649ba70ad5849cf0cd6598d0a71b930
정서희 <jsh001505@gmail.com>
2024-05-07 21:55:35 +0900

    Feature: 지도 이동하기 예제 추가

---
+++ components/KakaoMap/MarkerList/index.ts
@@ -0,0 +1,4 @@
+import KakaoMapMarkerListCodeTs from "./KakaoMapMarkerListTs.vue?raw";
+import KakaoMapMarkerListCodeJs from "./KakaoMapMarkerListJs.vue?raw";
+
+export { KakaoMapMarkerListCodeTs, KakaoMapMarkerListCodeJs };
--- pages/components/kakaoMap.vue
+++ pages/components/kakaoMap.vue
@@ -2,7 +2,14 @@
 import {
   KakaoMapBasicMapCodeTs,
   KakaoMapBasicMapCodeJs,
+  KakaoMapMoveMapCodeTs,
+  KakaoMapMoveMapCodeJs,
+  KakaoMapMapInfoCodeJs,
+  KakaoMapMapInfoCodeTs,
+  KakaoMapMarkerListCodeJs,
+  KakaoMapMarkerListCodeTs,
 } from "@/components/KakaoMap";
+import KakaoMapMarkerListTs from "~/components/KakaoMap/MarkerList/KakaoMapMarkerListTs.vue";
 
 const anchorItems = [
   {
@@ -20,10 +27,12 @@ const anchorItems = [
     href: "#map-info",
     title: "지도 정보 얻어오기",
   },
+  {
+    key: "marker-list",
+    href: "#marker-list",
+    title: "여러개의 마커 제어하기",
+  },
 ];
-
-const tsCode = KakaoMapBasicMapCodeTs as string;
-const jsCode = KakaoMapBasicMapCodeJs as string;
 </script>
 
 <template>
@@ -31,7 +40,7 @@ const jsCode = KakaoMapBasicMapCodeJs as string;
     <h1>KakaoMap</h1>
     <h2 id="basic-map">지도 생성하기</h2>
     <p>기본적인 지도 생성입니다.</p>
-    <CodeBox :tsCode="tsCode" :jsCode="jsCode">
+    <CodeBox :tsCode="KakaoMapBasicMapCodeTs" :jsCode="KakaoMapBasicMapCodeJs">
       <template #demo>
         <KakaoMapBasicMapJs />
       </template>
@@ -43,7 +52,7 @@ const jsCode = KakaoMapBasicMapCodeJs as string;
       이동시킬 수 있습니다. 또, 지도가 표시되고 있는 영역크기를 벗어나지 않는
       거리라면 애니메이션 효과처럼 지도를 부드럽게 이동시킬 수도 있습니다.
     </p>
-    <CodeBox :tsCode="tsCode" :jsCode="jsCode">
+    <CodeBox :tsCode="KakaoMapMoveMapCodeTs" :jsCode="KakaoMapMoveMapCodeJs">
       <template #demo>
         <KakaoMapMoveMapTs />
       </template>
@@ -53,9 +62,23 @@ const jsCode = KakaoMapBasicMapCodeJs as string;
     <NuxtLink href="https://apis.map.kakao.com/web/documentation/#MapTypeId"
       >지도 타입을 보시려면 여기를 클릭하세요!</NuxtLink
     >
-    <CodeBox :tsCode="tsCode" :jsCode="jsCode">
+    <CodeBox :tsCode="KakaoMapMapInfoCodeTs" :jsCode="KakaoMapMapInfoCodeJs">
       <template #demo>
         <KakaoMapMapInfoTs />
+      </template>
+    </CodeBox>
+
+    <h2 id="marker-list">여러개의 마커 제어하기</h2>
+    <p>지도 레벨, 중심좌표, 지도 타입, 지도 영역정보를 얻어와 표출합니다.</p>
+    <NuxtLink href="https://apis.map.kakao.com/web/documentation/#MapTypeId"
+      >지도 타입을 보시려면 여기를 클릭하세요!</NuxtLink
+    >
+    <CodeBox
+      :tsCode="KakaoMapMarkerListCodeTs"
+      :jsCode="KakaoMapMarkerListCodeJs"
+    >
+      <template #demo>
+        <KakaoMapMarkerListTs />
       </template>
     </CodeBox>
   </div>
