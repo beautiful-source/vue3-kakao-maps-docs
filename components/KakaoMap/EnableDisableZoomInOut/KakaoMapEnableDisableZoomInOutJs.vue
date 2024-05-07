b1c7d40397f839a54c385c56d601baea38b81c1f
양소영 <maysoyoung@gmail.com>
2024-05-07 18:03:17 +0900

    Feature: 지도 확대 축소 막기 추가

--- components/KakaoMap/EnableDisableZoomInOut/KakaoMapEnableDisableZoomInOutJs.vue
+++ components/KakaoMap/EnableDisableZoomInOut/KakaoMapEnableDisableZoomInOutJs.vue
@@ -1,14 +1,14 @@
-<script setup lang="ts">
+<script setup>
 import { KakaoMap } from "vue3-kakao-maps";
 
-const map = ref<kakao.maps.Map>();
+const map = ref();
 
-const onLoadKakaoMap = (mapRef: kakao.maps.Map) => {
+const onLoadKakaoMap = (mapRef) => {
   map.value = mapRef;
 };
 
 // 버튼 클릭에 따라 지도 확대, 축소 기능을 막거나 풀고 싶은 경우에는 map.value.setZoomable 함수를 사용합니다
-const setZoomable = (zoomable: boolean) => {
+const setZoomable = (zoomable) => {
   // 마우스 휠로 지도 확대,축소 가능여부를 설정합니다
   map.value?.setZoomable(zoomable);
 };
--- pages/components/kakaoMap.vue
+++ pages/components/kakaoMap.vue
@@ -2,6 +2,12 @@
 import {
   KakaoMapBasicMapCodeTs,
   KakaoMapBasicMapCodeJs,
+  KakaomapchangeLevelCodeTs,
+  KakaomapchangeLevelCodeJs,
+  KakaoMapDisableMapDragMoveCodeTs,
+  KakaoMapDisableMapDragMoveCodeJs,
+  KakaoMapEnableDisableZoomInOutCodeTs,
+  KakaoMapEnableDisableZoomInOutCodeJs,
 } from "@/components/KakaoMap";
 
 const anchorItems = [
@@ -16,9 +22,24 @@ const anchorItems = [
     title: "지도 이동시키기",
   },
   {
+    key: "change-level",
+    href: "#change-level",
+    title: "지도 레벨 바꾸기",
+  },
+  {
     key: "map-info",
     href: "#map-info",
     title: "지도 정보 얻어오기",
+  },
+  {
+    key: "disable-map-drag-move",
+    href: "#disable-map-drag-move",
+    title: "지도 이동 막기",
+  },
+  {
+    key: "enable-disable-zoom-in-out",
+    href: "#enable-disable-zoom-in-out",
+    title: "지도 확대 축소 막기",
   },
 ];
 
@@ -36,7 +57,6 @@ const jsCode = KakaoMapBasicMapCodeJs as string;
         <KakaoMapBasicMapJs />
       </template>
     </CodeBox>
-
     <h2 id="move-map">지도 이동시키기</h2>
     <p>
       지도를 이동시킵니다. 지도 객체의 메소드를 통해 지도를 원하는 좌표로
@@ -48,6 +68,16 @@ const jsCode = KakaoMapBasicMapCodeJs as string;
         <KakaoMapMoveMapTs />
       </template>
     </CodeBox>
+    <h2 id="change-level">지도 레벨 바꾸기</h2>
+    <p>거리라면 애니메이션 효과처럼 지도를 부드럽게 이동시킬 수도 있습니다.</p>
+    <CodeBox
+      :tsCode="KakaomapchangeLevelCodeTs"
+      :jsCode="KakaomapchangeLevelCodeJs"
+    >
+      <template #demo>
+        <KakaoMapChangeLevelTs />
+      </template>
+    </CodeBox>
     <h2 id="map-info">지도 정보 얻어오기</h2>
     <p>지도 레벨, 중심좌표, 지도 타입, 지도 영역정보를 얻어와 표출합니다.</p>
     <NuxtLink href="https://apis.map.kakao.com/web/documentation/#MapTypeId"
@@ -56,6 +86,33 @@ const jsCode = KakaoMapBasicMapCodeJs as string;
     <CodeBox :tsCode="tsCode" :jsCode="jsCode">
       <template #demo>
         <KakaoMapMapInfoTs />
+      </template>
+    </CodeBox>
+    <h2 id="disable-map-drag-move">지도 이동 막기</h2>
+    <p>
+      마우스 드래그로 지도를 이동시키는 기능을 막습니다. 모바일 기기에서
+      터치스크롤시 지도가 이동되는 것을 막고싶거나 지도가 이동되면 안되는 경우
+      등 상황에 따라 지도의 이동 기능을 제어할 수 있습니다.
+    </p>
+    <CodeBox
+      :tsCode="KakaoMapDisableMapDragMoveCodeTs"
+      :jsCode="KakaoMapDisableMapDragMoveCodeJs"
+    >
+      <template #demo>
+        <KakaoMapDisableMapDragMoveJs />
+      </template>
+    </CodeBox>
+    <h2 id="enable-disable-zoom-in-out">지도 확대 축소 막기</h2>
+    <p>
+      마우스 휠이나 멀티터치로 지도 확대, 축소 기능을 막습니다. 상황에 따라 지도
+      확대, 축소 기능을 제어할 수 있습니다.
+    </p>
+    <CodeBox
+      :tsCode="KakaoMapEnableDisableZoomInOutCodeTs"
+      :jsCode="KakaoMapEnableDisableZoomInOutCodeJs"
+    >
+      <template #demo>
+        <KakaoMapEnableDisableZoomInOutTs />
       </template>
     </CodeBox>
   </div>
