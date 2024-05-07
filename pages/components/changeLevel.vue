8b5382bc4f9e4501579c046c29d83fe96c30e3de
양소영 <maysoyoung@gmail.com>
2024-05-07 14:46:19 +0900

    Feature: 지도 레벨 바꾸기

---
+++ pages/components/changeLevel.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { KakaomapchangeLevelCodeJs } from "@/components/KakaoMap";
+
+const tsCode = KakaomapchangeLevelCodeJs as string;
+const jsCode = KakaomapchangeLevelCodeJs as string;
+</script>
+
+<template>
+  <h1>KakaoMap</h1>
+  <p>카카오맵 지도 레벨 변경하기 컴포넌트입니다.</p>
+  <CodeBox :tsCode="tsCode" :jsCode="jsCode">
+    <template #demo>
+      <KakaoMapChangeLevelJs />
+    </template>
+  </CodeBox>
+</template>
+
+<style scoped lang="scss"></style>
