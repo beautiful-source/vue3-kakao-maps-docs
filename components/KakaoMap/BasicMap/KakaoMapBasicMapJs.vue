6573e29fc5cb20c6ea00983041e066ef03ce6add
정서희 <jsh001505@naver.com>
2024-05-07 10:04:33 +0900

    Refactor: 기본 KakaoMap 태그를 셀프 클로징으로 수정

--- components/KakaoMap/BasicMap/KakaoMapBasicMapJs.vue
+++ components/KakaoMap/BasicMap/KakaoMapBasicMapJs.vue
@@ -3,7 +3,7 @@ import { KakaoMap } from "vue3-kakao-maps";
 </script>
 
 <template>
-  <KakaoMap :lat="33.450701" :lng="126.570667"> </KakaoMap>
+  <KakaoMap :lat="33.450701" :lng="126.570667" />
 </template>
 
 <style scoped></style>
