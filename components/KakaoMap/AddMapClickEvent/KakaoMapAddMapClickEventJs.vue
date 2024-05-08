21f86c7e31e716e94e5e9a2bcd04c06544e91091
양소영 <maysoyoung@gmail.com>
2024-05-08 14:02:18 +0900

    Refactor: var를 const로 수정

--- components/KakaoMap/AddMapClickEvent/KakaoMapAddMapClickEventJs.vue
+++ components/KakaoMap/AddMapClickEvent/KakaoMapAddMapClickEventJs.vue
@@ -1,16 +1,16 @@
 <script setup>
-import { ref } from "vue";
-import { KakaoMap } from "vue3-kakao-maps";
+import { ref } from 'vue';
+import { KakaoMap } from 'vue3-kakao-maps';
 
 const map = ref();
-const message = ref("");
+const message = ref('');
 
 const onLoadKakaoMap = (mapRef) => {
   map.value = mapRef;
 
-  kakao.maps.event.addListener(map.value, "click", function (mouseEvent) {
+  kakao.maps.event.addListener(map.value, 'click', function (mouseEvent) {
     // 클릭한 위도, 경도 정보를 가져옵니다
-    var latlng = mouseEvent.latLng;
+    const latlng = mouseEvent.latLng;
 
     message.value = `클릭한 위치의 위도는 ${latlng.getLat()} 이고, <br>`;
     message.value += `경도는 ${latlng.getLng()} 입니다`;
--- components/KakaoMap/AddMapClickEvent/KakaoMapAddMapClickEventTs.vue
+++ components/KakaoMap/AddMapClickEvent/KakaoMapAddMapClickEventTs.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { KakaoMap } from "vue3-kakao-maps";
+import { ref } from 'vue';
+import { KakaoMap } from 'vue3-kakao-maps';
 
 const map = ref<kakao.maps.Map>();
-const message = ref<string>("");
+const message = ref<string>('');
 
 const onLoadKakaoMap = (mapRef: kakao.maps.Map) => {
   map.value = mapRef;
 
   kakao.maps.event.addListener(
     map.value,
-    "click",
+    'click',
     function (mouseEvent: kakao.maps.event.MouseEvent) {
       // 클릭한 위도, 경도 정보를 가져옵니다
-      var latlng = mouseEvent.latLng;
+      const latlng = mouseEvent.latLng;
 
       message.value = `클릭한 위치의 위도는 ${latlng.getLat()} 이고, <br>`;
       message.value += `경도는 ${latlng.getLng()} 입니다`;
