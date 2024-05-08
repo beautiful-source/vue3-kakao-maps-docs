93de91944a1beafe00b620c097314aa22f4cd69e
윤진아 <chris2761@naver.com>
2024-05-08 11:08:25 +0900

    Feature: customOverlay docs 개발 중

--- pages/components/kakaoMapCustomOverlay.vue
+++ pages/components/kakaoMapCustomOverlay.vue
@@ -1,7 +1,39 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+  KakaoMapBasicMapCodeTs,
+  KakaoMapBasicMapCodeJs,
+  KakaoMapBasicMap,
+} from '@/components/KakaoMap';
+
+const anchorItems = [
+  {
+    key: 'basic-map',
+    href: '#basic-map',
+    title: '지도 생성하기',
+  },
+  {
+    key: 'move-map',
+    href: '#move-map',
+    title: '지도 이동시키기',
+  },
+  {
+    key: 'map-info',
+    href: '#map-info',
+    title: '지도 정보 얻어오기',
+  },
+];
+
+const tsCode = KakaoMapBasicMapCodeTs as string;
+const jsCode = KakaoMapBasicMapCodeJs as string;
+</script>
 
 <template>
-  <div></div>
+  <div>
+    <h1>KakaoMapCustomOverlay</h1>
+    <p>카카오 커스텀 오버레이 컴포넌트입니다.</p>
+    <KakaoMapBasicMap />
+  </div>
+  <div class="anchor">
+    <a-anchor :items="anchorItems" />
+  </div>
 </template>
-
-<style scoped></style>
