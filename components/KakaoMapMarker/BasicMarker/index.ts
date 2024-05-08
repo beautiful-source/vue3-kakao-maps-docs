e46240e13eb1c5aed7075bed8edd8e5969850301
전소민 <goflvhxj96@naver.com>
2024-05-08 12:55:40 +0900

    Feature: Marker 폴더 구조 생성

---
+++ components/KakaoMapMarker/BasicMarker/index.ts
@@ -0,0 +1,4 @@
+import KakaoMapBasicMarkerCodeTs from "./KakaoMapBasicMarkerTs.vue?raw";
+import KakaoMapBasicMarkerCodeJs from "./KakaoMapBasicMarkerJs.vue?raw";
+
+export { KakaoMapBasicMarkerCodeTs, KakaoMapBasicMarkerCodeJs };
--- pages/components/kakaoMapMarker.vue
+++ pages/components/kakaoMapMarker.vue
@@ -1,7 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+  KakaoMapBasicMarkerCodeTs,
+  KakaoMapBasicMarkerCodeJs,
+} from "@/components/KakaoMapMarker";
+import KakaoMapBasicMarkerTs from "@/components/KakaoMapMarker/BasicMarker/KakaoMapBasicMarkerTs.vue";
+
+const anchorItems = [
+  {
+    key: "basic-marker",
+    href: "#basic-marker",
+    title: "마커 생성하기",
+  },
+];
+</script>
 
 <template>
-  <div></div>
+  <div>
+    <h1>KakaoMapMarker</h1>
+    <h2 id="basic-marker">마커 생성하기</h2>
+    <p>기본적인 마커 생성입니다.</p>
+    <CodeBox
+      :tsCode="KakaoMapBasicMarkerCodeTs"
+      :jsCode="KakaoMapBasicMarkerCodeJs"
+    >
+      <template #demo>
+        <KakaoMapBasicMarkerTs />
+      </template>
+    </CodeBox>
+  </div>
+  <div class="anchor">
+    <a-anchor :items="anchorItems" />
+  </div>
 </template>
-
-<style scoped></style>
