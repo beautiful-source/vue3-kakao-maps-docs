16680bc75785453338e323984059de72ffd7aff7
정서희 <jsh001505@gmail.com>
2024-05-08 22:06:20 +0900

    Feature: KakaoMapMarker도 레이아웃 적용

--- pages/components/kakaoMapMarker.vue
+++ pages/components/kakaoMapMarker.vue
@@ -35,50 +35,52 @@ const anchorItems = [
 </script>
 
 <template>
-  <div>
-    <h1>KakaoMapMarker</h1>
-    <h2 id="basic-marker">마커 생성하기</h2>
-    <p>기본적인 마커 생성입니다.</p>
-    <CodeBox
-      :tsCode="KakaoMapMarkerBasicMarkerCodeTs"
-      :jsCode="KakaoMapMarkerBasicMarkerCodeJs"
-    >
-      <template #demo>
-        <KakaoMapMarkerBasicMarkerTs />
-      </template>
-    </CodeBox>
-    <h2 id="image-marker">이미지 마커 생성하기</h2>
-    <p>이미지 마커 생성입니다.</p>
-    <CodeBox
-      :tsCode="KakaoMapMarkerImageMarkerCodeTs"
-      :jsCode="KakaoMapMarkerImageMarkerCodeJs"
-    >
-      <template #demo>
-        <KakaoMapMarkerImageMarkerTs />
-      </template>
-    </CodeBox>
-    <h2 id="infowindow-marker-1">인포윈도우가 있는 마커 생성하기 1</h2>
-    <p>props로 추가하는 예시입니다.</p>
-    <CodeBox
-      :tsCode="KakaoMapMarkerInfoWindowMarker1CodeTs"
-      :jsCode="KakaoMapMarkerInfoWindowMarker1CodeJs"
-    >
-      <template #demo>
-        <KakaoMapMarkerInfoWindowMarker1Ts />
-      </template>
-    </CodeBox>
-    <h2 id="infowindow-marker-2">인포윈도우가 있는 마커 생성하기 2</h2>
-    <p>v-slot으로 추가하는 예시입니다.</p>
-    <CodeBox
-      :tsCode="KakaoMapMarkerInfoWindowMarker2CodeTs"
-      :jsCode="KakaoMapMarkerInfoWindowMarker2CodeJs"
-    >
-      <template #demo>
-        <KakaoMapMarkerInfoWindowMarker2Ts />
-      </template>
-    </CodeBox>
-  </div>
-  <div class="anchor">
-    <a-anchor :items="anchorItems" />
+  <div class="contents-wrap">
+    <div class="contents">
+      <h1>KakaoMapMarker</h1>
+      <h2 id="basic-marker">마커 생성하기</h2>
+      <p>기본적인 마커 생성입니다.</p>
+      <CodeBox
+        :tsCode="KakaoMapMarkerBasicMarkerCodeTs"
+        :jsCode="KakaoMapMarkerBasicMarkerCodeJs"
+      >
+        <template #demo>
+          <KakaoMapMarkerBasicMarkerTs />
+        </template>
+      </CodeBox>
+      <h2 id="image-marker">이미지 마커 생성하기</h2>
+      <p>이미지 마커 생성입니다.</p>
+      <CodeBox
+        :tsCode="KakaoMapMarkerImageMarkerCodeTs"
+        :jsCode="KakaoMapMarkerImageMarkerCodeJs"
+      >
+        <template #demo>
+          <KakaoMapMarkerImageMarkerTs />
+        </template>
+      </CodeBox>
+      <h2 id="infowindow-marker-1">인포윈도우가 있는 마커 생성하기 1</h2>
+      <p>props로 추가하는 예시입니다.</p>
+      <CodeBox
+        :tsCode="KakaoMapMarkerInfoWindowMarker1CodeTs"
+        :jsCode="KakaoMapMarkerInfoWindowMarker1CodeJs"
+      >
+        <template #demo>
+          <KakaoMapMarkerInfoWindowMarker1Ts />
+        </template>
+      </CodeBox>
+      <h2 id="infowindow-marker-2">인포윈도우가 있는 마커 생성하기 2</h2>
+      <p>v-slot으로 추가하는 예시입니다.</p>
+      <CodeBox
+        :tsCode="KakaoMapMarkerInfoWindowMarker2CodeTs"
+        :jsCode="KakaoMapMarkerInfoWindowMarker2CodeJs"
+      >
+        <template #demo>
+          <KakaoMapMarkerInfoWindowMarker2Ts />
+        </template>
+      </CodeBox>
+    </div>
+    <div class="anchor">
+      <a-anchor :items="anchorItems" :offsetTop="20" />
+    </div>
   </div>
 </template>
