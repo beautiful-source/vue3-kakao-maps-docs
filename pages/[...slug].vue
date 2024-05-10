824df15715f013495d60f65bab53d1eaf3fc3c36
류기현 <112453419+geekseal@users.noreply.github.com>
2024-05-10 09:32:20 +0900

    Fix: Docs 스타일링 id로 적용

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,3 +1,68 @@
 <template>
-  <ContentDoc />
+  <ContentDoc id="nuxt-content" />
 </template>
+
+<style lang="scss">
+// Docs(nuxt-content) 한정 스타일
+div#nuxt-content {
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    a {
+      color: inherit;
+      position: relative;
+    }
+    a::before {
+      content: '';
+    }
+    a:hover::before {
+      content: '#';
+      position: absolute;
+      left: -16px;
+      top: 50%;
+      transform: translateY(-50%);
+      font-size: 16px;
+      color: $purple-6;
+    }
+  }
+
+  a {
+    text-decoration: none;
+    color: $purple-5;
+    transition: 0.4s;
+  }
+  a:hover {
+    background-color: $purple-1;
+  }
+
+  code {
+    color: $blue-7;
+    background-color: $blue-1;
+    font-size: 0.9rem;
+    padding: 2px 4px;
+    border-radius: 2px;
+  }
+
+  *[class='code-wrap'] {
+    pre,
+    code {
+      background-color: $code-bg-color;
+    }
+    pre {
+      padding: 1rem;
+      border-radius: 5px;
+      border: 2px solid $gray-4;
+      overflow-x: auto;
+
+      code {
+        padding: initial;
+        display: inline-block;
+        height: max-content;
+      }
+    }
+  }
+}
+</style>
