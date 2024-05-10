a661eb932ae2d53f8c5ab38c978c533594fdcfc7
양소영 <maysoyoung@gmail.com>
2024-05-10 16:20:56 +0900

    Refactor: data 타입 수정

--- components/KakaoMapMarker/KeywordBasic/KakaoMapMarkerKeywordBasicTs.vue
+++ components/KakaoMapMarker/KeywordBasic/KakaoMapMarkerKeywordBasicTs.vue
@@ -15,7 +15,7 @@ const onLoadKakaoMap = (mapRef: kakao.maps.Map) => {
 };
 
 // 키워드 검색 완료 시 호출되는 콜백함수 입니다
-const placesSearchCB = (data: any, status: kakao.maps.services.Status): void => {
+const placesSearchCB = (data: kakao.maps.services.PlacesSearchResult, status: kakao.maps.services.Status): void => {
   if (status === kakao.maps.services.Status.OK) {
     // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
     // LatLngBounds 객체에 좌표를 추가합니다
@@ -31,7 +31,7 @@ const placesSearchCB = (data: any, status: kakao.maps.services.Status): void => 
         }
       };
       markerList.value.push(markerItem);
-      bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+      bounds.extend(new kakao.maps.LatLng(Number(data[i].y), Number(data[i].x)));
     }
 
     // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
