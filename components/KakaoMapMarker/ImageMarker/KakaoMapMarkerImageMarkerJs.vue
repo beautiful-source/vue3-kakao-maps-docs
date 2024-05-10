35c40bb062244b60567bf6ecbd9d2eb6196cedeb
양소영 <maysoyoung@gmail.com>
2024-05-10 09:52:15 +0900

    Refactor: 마커 이미지 생성 상대경로

--- components/KakaoMapMarker/ImageMarker/KakaoMapMarkerImageMarkerJs.vue
+++ components/KakaoMapMarker/ImageMarker/KakaoMapMarkerImageMarkerJs.vue
@@ -7,8 +7,20 @@ import { KakaoMap, KakaoMapMarker } from 'vue3-kakao-maps';
     <KakaoMapMarker
       :lat="33.450705"
       :lng="126.570667"
+      title="절대경로로 이미지 가져오기"
       :image="{
         imageSrc: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
+        imageWidth: 64,
+        imageHeight: 64,
+        imageOption: {}
+      }"
+    />
+    <KakaoMapMarker
+      :lat="33.450705"
+      :lng="126.572367"
+      title="상대경로로 이미지 가져오기"
+      :image="{
+        imageSrc: '/images/markerImage.png',
         imageWidth: 64,
         imageHeight: 64,
         imageOption: {}
