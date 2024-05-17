66cc59cc89f64fa40100f8c493cfa619085a8fef
윤진아 <1891071@naver.com>
2024-05-17 14:33:40 +0900

    Fix: BasicPolyline docs 수정, 컴포넌트화

--- components/KakaoMapPolyline/BasicPolyline/KakaoMapPolylineBasicPolyline.vue
+++ components/KakaoMapPolyline/BasicPolyline/KakaoMapPolylineBasicPolyline.vue
@@ -1,36 +1,33 @@
 <script setup lang="ts">
-import { KakaoMap, type KakaoMapMarkerListItem, KakaoMapMarkerPolyline } from 'vue3-kakao-maps';
+import { KakaoMap, KakaoMapPolyline } from 'vue3-kakao-maps';
+import type { KakaoMapLatLngItem } from 'vue3-kakao-maps';
 import type { ControlDataSourceType } from '@/components/common/ControlBox.vue';
 import type { HandlerBoxDataSourceType } from '@/components/common/HandlerBox.vue';
 
-const markerList = ref<KakaoMapMarkerListItem[]>([
-  {
-    lat: 33.4509,
-    lng: 126.571
-  },
-  { lat: 33.451, lng: 126.572 },
-  { lat: 33.452, lng: 126.573 },
-  { lat: 33.4518, lng: 126.5725 }
-]);
 const endArrow = ref<boolean>(false);
 const strokeWeight = ref<number>(3);
 const strokeColor = ref<string>('#F10000');
 const strokeOpacity = ref<number>(0.6);
 const strokeStyle = ref<kakao.maps.StrokeStyles>('solid');
 const zIndex = ref<number>();
-const showMarkerOrder = ref<boolean>(true);
 
+const latLngList = ref<KakaoMapLatLngItem[]>([
+  { lat: 33.45, lng: 126.571 },
+  { lat: 33.449, lng: 126.5705 },
+  { lat: 33.45, lng: 126.5725 }
+]);
 const controlData: Ref<ControlDataSourceType[]> = ref([
   {
-    name: 'markerList',
-    description: '지도에 표시할 marker 데이터의 리스트',
-    required: true,
-    type: ['KakaoMapMarkerListItem[]']
+    name: 'latLngList',
+    description: '폴리라인이 지나갈 경로',
+    type: ['KakaoMapLatLngItem[]'],
+    required: true
   },
   {
     name: 'endArrow',
-    description: '폴리라인의 화살표 여부',
+    description: '선의 화살표 여부',
     type: ['boolean'],
+    default: 'false',
     control: {
       type: 'Boolean',
       value: endArrow
@@ -38,17 +35,17 @@ const controlData: Ref<ControlDataSourceType[]> = ref([
   },
   {
     name: 'strokeWeight',
-    description: '폴리라인의 두께',
+    description: '선의 두께',
     type: ['number'],
     default: 3,
     control: {
-      type: 'Input',
+      type: 'NumberInput',
       value: strokeWeight
     }
   },
   {
     name: 'strokeColor',
-    description: '폴리라인의 색',
+    description: '선의 색',
     type: ['string'],
     default: '#F10000',
     control: {
@@ -58,18 +55,17 @@ const controlData: Ref<ControlDataSourceType[]> = ref([
   },
   {
     name: 'strokeOpacity',
-    description: '폴리라인의 불투명도. 1에서 0 사이의 값이며 0에 가까울수록 투명하다.',
+    description: '선의 불투명도. 1에서 0 사이의 값이며 0에 가까울수록 투명하다',
     type: ['number'],
     default: 0.6,
     control: {
-      type: 'Input',
+      type: 'NumberInput',
       value: strokeOpacity
     }
   },
   {
     name: 'strokeStyle',
-    description:
-      '폴리라인의 스타일로 총 11종류가 있다. (solid, shortdash, shortdot, shortdashdot, shortdashdotdot, dot, dash, dashdot, longdash, longdashdot, longdashdotdot)',
+    description: '선의 스타일',
     type: ['kakao.maps.StrokeStyles'],
     default: 'solid',
     control: {
@@ -79,23 +75,15 @@ const controlData: Ref<ControlDataSourceType[]> = ref([
   },
   {
     name: 'zIndex',
-    description: '폴리라인의 z-index 속성 값',
+    description: '선의 z-index 속성 값',
     type: ['number'],
     control: {
-      type: 'Input',
+      type: 'NumberInput',
       value: zIndex
-    }
-  },
-  {
-    name: 'showMarkerOrder',
-    description: '마커의 순서 여부',
-    type: ['boolean'],
-    control: {
-      type: 'Boolean',
-      value: showMarkerOrder
     }
   }
 ]);
+
 const eventsData: Ref<HandlerBoxDataSourceType[]> = ref([
   {
     name: 'onLoadKakaoMapPolyline',
@@ -107,16 +95,15 @@ const eventsData: Ref<HandlerBoxDataSourceType[]> = ref([
 
 <template>
   <BasicComponentBox :control-data="controlData" :events-data="eventsData">
-    <KakaoMap :lat="33.452" :lng="126.572">
-      <KakaoMapMarkerPolyline
-        :markerList="markerList"
-        :endArrow="endArrow"
-        :strokeWeight="strokeWeight"
-        :strokeColor="strokeColor"
+    <KakaoMap :lat="33.450701" :lng="126.570667">
+      <KakaoMapPolyline
+        :latLngList="latLngList"
         :strokeOpacity="strokeOpacity"
+        :strokeColor="strokeColor"
         :strokeStyle="strokeStyle"
+        :strokeWeight="strokeWeight"
         :zIndex="zIndex"
-        :showMarkerOrder="showMarkerOrder"
+        :endArrow="endArrow"
       />
     </KakaoMap>
   </BasicComponentBox>
