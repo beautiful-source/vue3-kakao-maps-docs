168a594f2cf52dd7280ce46e83ad922d1824ff06
윤진아 <chris2761@naver.com>
2024-05-23 15:03:20 +0900

    Fix: control 반응형 제거

--- components/common/BasicComponentBox.vue
+++ components/common/BasicComponentBox.vue
@@ -13,9 +13,7 @@ const { controlData, eventsData } = defineProps<BasicComponentBoxProps>();
 <template>
   <div class="control-div">
     <slot></slot>
-    <div class="control-box-div">
-      <ControlBox v-bind:dataSource="controlData" />
-    </div>
+    <ControlBox v-bind:dataSource="controlData" />
   </div>
   <div v-if="eventsData">
     <p class="emit-event-title">Events</p>
@@ -30,20 +28,8 @@ const { controlData, eventsData } = defineProps<BasicComponentBoxProps>();
   width: 100%;
   height: 100%;
   color: $gray-13;
-  @media (max-width: $media-breakpoint-xlarge) {
-    flex-direction: column;
-  }
 }
 
-.control-box-div {
-  width: 60%;
-  margin-left: 10px;
-  @media (max-width: $media-breakpoint-xlarge) {
-    width: 100%;
-    margin-left: 0px;
-    margin-top: 10px;
-  }
-}
 .emit-event-title {
   font-size: 1.4rem;
   font-weight: 600;
